fa1a91a31da5ef682bfdd26356690be72854a61d
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2024-01-15 21:05:04 +0300

    fdg
    sdf

--- ProjectSite/appPages/urls.py
+++ ProjectSite/appPages/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'home/?$', views.HomePage.as_view(), name="url-home"),
+    path('', views.HomePage.as_view(), name="url-home"),
     re_path(r'news/?$', views.NewsPage.as_view(), name="url-news"),
+    re_path(r'senf/?$', views.SenfPage.as_view(), name="url-senf"),
+    re_path(r'contact/?$', views.ContactPage.as_view(), name="url-contact"),
 ]
--- ProjectSite/appPages/views.py
+++ ProjectSite/appPages/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import News
 
 class HomePage(View):
     def get(self, request):
-        return render(request, 'appPages/home/index.html')
+        data = {
+            'block_css':'home',
+        }
+        return render(request, 'appPages/home/index.html', data)
 class NewsPage(View):
     def get(self, request):
-        return render(request, 'appPages/news/index.html')
+        data = {
+            'block_css':'news',
+            'list_news': News.objects.order_by('-date')
+        }
+        return render(request, 'appPages/news/index.html', data)
+class SenfPage(View):
+    def get(self, request):
+        data = {
+            'header':{
+                'block_css':'senf',
+            }
+        }
+        return render(request, 'appPages/senf/index.html', data)
+class ContactPage(View):
+    def get(self, request):
+        data = {
+            'block_css':'contact',
+        }
+        return render(request, 'appPages/contact/index.html', data)
