64c3988fb6a7d14b2884181886acd34ab88eecc4
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2024-01-29 21:04:08 +0300

    Добавление связей для продукта и категории | Test Проект

--- TestProject/appKey/admin.py
+++ TestProject/appKey/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    list_display_links = ['title']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'category', 'price']
+    list_display_links = ['title', 'category']
