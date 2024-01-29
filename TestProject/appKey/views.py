13622305277abe1804af0a6fcf25b39f479571b7
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2024-01-29 19:58:39 +0300

    Документы+Новый Проект
    Добавление страницы документов + Создание тестового проекта

---
+++ TestProject/appKey/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import JsonResponse # -> отображать словарь для API
+from django.views import View
+
+class HomeKey(View):
+    def get(self, request):
+        return JsonResponse({
+            'status':200
+        })
