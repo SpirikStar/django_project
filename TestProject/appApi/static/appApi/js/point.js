636460a6d8bd3c0bff91cb34f4e42bb6710017f8
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2024-02-14 21:21:39 +0300

    fix

--- TestProject/appApi/static/appApi/js/point.js
+++ TestProject/appApi/static/appApi/js/point.js
@@ -1,22 +1,44 @@
 $(document).ready(function () {
+    $('#form_generate_email').submit(function (e) {
+        e.preventDefault();
+        let form = $(this)
+        let len = form.find('input[name="len"]').val()
+        let csrftoken = form.find('input[name="csrfmiddlewaretoken"]').val();
+        $.ajax({
+            type: 'POST',
+            url: '/api',
+            data: {
+                count_email: len,
+                action: 'generate-email',
+                csrfmiddlewaretoken: csrftoken
+            },
+            success: function (response) {
+
+            },
+            error: function (response) { }
+        })
+
+    })
+
     $('.send-btn').click(function () {
         $.ajax({
             type: 'POST',
             url: '/api',
             data: {
-                len_password:$('#len_password').val(),
-                count_password:$('#count_password').val(),
+                action: 'generate-password',
+                len_password: $('#len_password').val(),
+                count_password: $('#count_password').val(),
                 csrfmiddlewaretoken: $('meta[name="csrf-token"]').attr("content")
             },
-            success: function (response){
-                for (let i = 0; i < response.data_password.length; i++){
+            success: function (response) {
+                for (let i = 0; i < response.data_password.length; i++) {
                     let section = document.querySelector('#passwords')
                     let p = document.createElement('p')
                     p.textContent = response.data_password[i]
                     section.appendChild(p)
                 }
             },
-            error: function(response){}             
+            error: function (response) { }
         })
     })
 })
--- TestProject/appApi/views.py
+++ TestProject/appApi/views.py
@@ -2,23 +2,45 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 import random
+from fake_email import Email
+
 
 class ApiHome(View):
     def get(self, request):
         return render(request, 'appApi/index.html')
+
     def post(self, request):
+        action = request.POST['action']
+        if action == 'generate-password':
+            data = self.generate_password(request)
+        elif action == 'generate-email':
+            data = self.generate_email(request)
+        return JsonResponse(data)
+
+    def generate_email(self, request):
+        data_email = []
+        count_email = request.POST['count_email']
+        for i in range(int(count_email)):
+            mail=Email().Mail()['mail']
+            data_email.append(mail)
+        return {
+            'status': 'ok',
+            'data_email': data_email
+        }
+
+    
+    def generate_password(self, request):
         count_password = request.POST['count_password']
         len_password = request.POST['len_password']
         data_chars = 'QWERTYUIOPASDFGHJKLZXCVBNM123456789qwertyuiopasdfghjklzxcvbnm'
         data = []
-        
-        # random.choice([main.ru, 'ya.ru'])
+
         for psw in range(int(count_password)):
             password = ''
             for char in range(int(len_password)):
                 password += data_chars[random.randint(0, len(data_chars)-1)]
             data.append(password)
-        return JsonResponse({
-            'status':'ok', 
+        return {
+            'status': 'ok',
             'data_password': data
-        })
+        }
