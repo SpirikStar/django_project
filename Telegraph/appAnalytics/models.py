7664bbeb1f2b70a80af46c16679da283fb190284
Alexey Spiridonov <92844309+SpirikStar@users.noreply.github.com>
2024-03-06 21:10:58 +0300

    history

--- Telegraph/appAnalytics/models.py
+++ Telegraph/appAnalytics/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from appPublication.models import PubNews
 
-# Create your models here.
+
+class StaticLInk(models.Model):
+    pub_news = models.ForeignKey(
+        PubNews,
+        on_delete=models.CASCADE,
+        null=False,
+        verbose_name="Публикация"
+    )
+    os = models.CharField(
+        verbose_name="Операцационная система",
+        null=True,
+        blank=True,
+        max_length=150
+    )
+    browser = models.CharField(
+        verbose_name="Браузер",
+        null=True,
+        blank=True,
+        max_length=150
+    )
+    is_bot = models.BooleanField(
+        verbose_name="Бот",
+        choices=[
+            (True, "Да"),
+            (False, "Нет"),
+        ],
+        default=False,
+        null=True,
+        blank=True,
+    )
+    is_touch_capable = models.BooleanField(
+        verbose_name="Поддержка сенсорного экрана",
+        choices=[
+            (True, "Да"),
+            (False, "Нет"),
+            (False, "Нет"),
+        ],
+        default=False,
+        null=True,
+        blank=True,
+    )
+    type_device = models.CharField(
+        verbose_name="Тип устройства",
+        choices=[
+            ('mobile', "Телефон"),
+            ('tablet', "Планшет"),
+            ('pc', "ПК"),
+        ],
+        default=False,
+        null=True,
+        blank=True,
+        max_length=150
+    )
+    def __str__(self):
+        return self.pub_news.name_url
+    class Meta:
+        verbose_name="историю"
+        verbose_name_plural="Просмотры"
--- Telegraph/appPublication/urls.py
+++ Telegraph/appPublication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.PublicationPage.as_view(), name="urlPublicationPage")
+    path('', views.PublicationPage.as_view(), name="urlPublicationPage"),
+    path('<slug:name>', views.OpenPublicPage.as_view(), name="urlOpenPublicPage"),
 ]
